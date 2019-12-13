27f94a8b02aad399a77984750d1e72873ccc71e6
Antoine Folie <antoine.folie-plaineau@epitech.eu>
2019-12-13 13:46:41 +0100

    [FIX] Fixed linter and typo on doctor dashboard

--- b.bis_web/src/components/DoctorDashboard.tsx
+++ b.bis_web/src/components/DoctorDashboard.tsx
@@ -5,13 +5,17 @@ import {
   Typography,
   createStyles,
   makeStyles,
+  Grid,
 } from '@material-ui/core';
 import useDefaultStore from '../context/storeApi';
 import Chart from './Chart/chart';
 
 const useStyles = makeStyles((theme) => createStyles({
-  root: {
+  paper: {
     padding: theme.spacing(3, 2),
+  },
+  root: {
+    flewGrow: 1,
   },
 }));
 
@@ -21,16 +25,40 @@ export default function Test() {
   const { login } = useDefaultStore();
 
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        Hello
-        {' '}
-        { login }
-      </Typography>
-      <Typography component="p">
-        Paper can be used to build surface or other elements for your application.
-      </Typography>
-      <Chart />
-    </Paper>
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        alignItems="center"
+        direction="column"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5" component="h3" align="center">
+              Patient
+              {' '}
+              { login }
+            </Typography>
+            <Typography component="p" variant="h4" align="center">
+              Antoine, 21 ans, développeur
+            </Typography>
+            <Typography component="p" variant="body1">
+              Patient depuis 2 ans
+            </Typography>
+            <Typography component="p" variant="body1">
+              Gastroentérologue: Dr. Chiche
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography component="h2" variant="h4" align="center">
+            Échelle de douleur des crises sur les 2 derniers mois.
+          </Typography>
+          <Chart />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
